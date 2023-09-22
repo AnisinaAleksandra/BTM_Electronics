cab644a7b34e06f84c99eac8cf08d04d1f789165
Anisina <makhmutovansk@gmail.com>
2023-09-22 19:56:21 +0300

    add contacts part in app

---
+++ src/components/FormComponent/index.ts
@@ -0,0 +1,3 @@
+import { FormComponentAsync } from "./ui/FormComponent.async";
+
+export { FormComponentAsync as FormComponent };
--- src/pages/Contacts/ui/Contacts.tsx
+++ src/pages/Contacts/ui/Contacts.tsx
@@ -1,15 +1,52 @@
 import { MapY } from "../../../widget/MapY";
 import style from "./Contacts.module.scss";
-
+import svgAdress from "../../../assets/images/address.svg";
+import svgPhone from "../../../assets/images/phone.svg";
+import svgEmail from "../../../assets/images/email.svg";
+import { FormComponent } from "../../../components/FormComponent";
 const Contacts = () => {
   return (
     <div className={style.Contacts}>
       <div className={style.title}>Contact Us</div>
-      <div className={style.map_messageForm}>
+      <div className={style.info_cards_list}>
+        <div className={style.info_card}>
+          <div className={style.icon}>
+            <img src={svgAdress} />
+          </div>
+          <div className={style.description}>
+            <div className={style.title_card}>Our adress</div>
+            <div className={style.text_card}>
+              Dubai Airport Free Zone, 9WA G077
+            </div>
+          </div>
+        </div>
+        <div className={style.info_card}>
+          <div className={style.icon}>
+            <img src={svgEmail} />
+          </div>
+          <div className={style.description}>
+            <div className={style.title_card}>Our email</div>
+            <div className={style.text_card}>btmelectronics@gmail.com</div>
+          </div>
+        </div>
+        <div className={style.info_card}>
+          <div className={style.icon}>
+            <img src={svgPhone} />
+          </div>
+          <div className={style.description}>
+            <div className={style.title_card}>Our phone</div>
+            <div className={style.text_card}>+995591052466</div>
+            <div className={style.text_card}> @usediphonesforsale</div>
+          </div>
+        </div>
+      </div>
+      <div className={style.map_message_form}>
         <div className={style.map}>
           <MapY />
         </div>
-        <div className={style.messageForm}></div>
+        <div className={style.messageForm}>
+          <FormComponent />
+        </div>
       </div>
     </div>
   );
