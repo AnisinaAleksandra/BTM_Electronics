0c2f55ef55353687997295bd7b58e5218e7fafe5
Anisina <makhmutovansk@gmail.com>
2023-09-29 01:49:47 +0300

    add menu style for mobile versions

--- src/widget/Header/ui/Header.tsx
+++ src/widget/Header/ui/Header.tsx
@@ -2,7 +2,13 @@ import { Link } from "react-scroll";
 
 import style from "./Header.module.scss";
 import logoPath from "../../../assets/images/BTMLogo.png";
+import classNames from "classnames/bind";
+import { useState } from "react";
+
+const cx = classNames.bind(style);
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
   document.querySelectorAll("li").forEach((elem) => {
     elem.onmouseenter = elem.onmouseleave = (e) => {
       const tolerance = 10;
@@ -18,13 +24,21 @@ const Header = () => {
       elem.style.setProperty("--x", `${x}px`);
     };
   });
+
+  const openMenu = (): void => {
+    setOpen((prev) => !prev);
+  };
+
   return (
     <div className={style.Header}>
       <div className={style.container}>
         <div className={style.logo_container}>
           <img src={logoPath} alt="logo" />
         </div>
-        <nav className={style.menu}>
+        <nav
+          className={style.menu}
+          style={{ transform: open ? "translateX(0)" : "translateX(150%)" }}
+        >
           <ul>
             <li className={style.menu_item}>
               {" "}
@@ -78,6 +92,20 @@ const Header = () => {
             </li>
           </ul>
         </nav>
+
+        <div className={style.burger_container} onClick={openMenu}>
+          <input
+            className={style.checkbox}
+            type="checkbox"
+            name=""
+            id="checkbox"
+          />
+          <div className={style.hamburgerLines}>
+            <span className={cx("line", ["line1"])}></span>
+            <span className={cx("line", ["line2"])}></span>
+            <span className={cx("line", ["line3"])}></span>
+          </div>
+        </div>
         <div className={style.btns_container}>
           <a className={style.whatsapp} href="https://wa.me/971585987809">
             WhatsApp
