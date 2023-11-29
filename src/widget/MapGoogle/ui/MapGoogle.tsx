fe414ae4e2e2cb2342c33aeec67c1a1e17f3c6a9
Anisina <makhmutovansk@gmail.com>
2023-11-30 01:54:26 +0400

    add seo meta for whatsap

--- src/widget/MapGoogle/ui/MapGoogle.tsx
+++ src/widget/MapGoogle/ui/MapGoogle.tsx
@@ -1,35 +1,166 @@
 import style from "./MapGoogle.module.scss";
 
 import GoogleMapReact from "google-map-react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+// import { createCustomEqual } from "fast-equals";
+import { isLatLngLiteral } from "@googlemaps/typescript-guards";
+import React from "react";
 
-const AnyReactComponent = ({
-  text,
-}: {
-  lat: number;
-  lng: number;
-  text: string;
-}) => <div>{text}</div>;
+interface MapProps extends google.maps.MapOptions {
+  style: { [key: string]: string };
+  onClick?: (e: google.maps.MapMouseEvent) => void;
+  onIdle?: (map: google.maps.Map) => void;
+  children?: React.ReactNode;
+}
 
-export const MapGoogle = () => {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
+const Map: React.FC<MapProps> = ({
+  onClick,
+  onIdle,
+  children,
+  style,
+  ...options
+}) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const [map, setMap] = React.useState<google.maps.Map>();
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+
+  // because React does not do deep comparisons, a custom hook is used
+  // see discussion in https://github.com/googlemaps/js-samples/issues/946
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options);
+    }
+  }, [map, options]);
+
+  React.useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) =>
+        google.maps.event.clearListeners(map, eventName)
+      );
+
+      if (onClick) {
+        map.addListener("click", onClick);
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+  }, [map, onClick, onIdle]);
 
   return (
-    // Important! Always set the container height explicitly
-    <div className={style.MapGoogle}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        yesIWantToUseGoogleMapApiInternals
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-      </GoogleMapReact>
-    </div>
+    <>
+      <div ref={ref} style={style} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          // set the map prop on the child component
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          return React.cloneElement(child, { map });
+        }
+      })}
+    </>
   );
 };
+
+const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
+  const [marker, setMarker] = React.useState<google.maps.Marker>();
+
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new google.maps.Marker());
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+
+  return null;
+};
+
+const deepCompareEqualsForMaps = createCustomEqual(
+  (deepEqual) => (a: any, b: any) => {
+    if (
+      isLatLngLiteral(a) ||
+      a instanceof google.maps.LatLng ||
+      isLatLngLiteral(b) ||
+      b instanceof google.maps.LatLng
+    ) {
+      return new google.maps.LatLng(a).equals(new google.maps.LatLng(b));
+    }
+
+    // TODO extend to other types
+
+    // use fast-equals for other objects
+    return deepEqual(a, b);
+  }
+);
+
+function useDeepCompareMemoize(value: any) {
+  const ref = React.useRef();
+
+  if (!deepCompareEqualsForMaps(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+function useDeepCompareEffectForMaps(
+  callback: React.EffectCallback,
+  dependencies: any[]
+) {
+  React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const root = createRoot(document.getElementById("root")!);
+  root.render(<App />);
+});
+
+// const AnyReactComponent = ({
+//   text,
+// }: {
+//   lat: number;
+//   lng: number;
+//   text: string;
+// }) => <div>{text}</div>;
+
+// export const MapGoogle = () => {
+//   const defaultProps = {
+//     center: {
+//       lat: 10.99835602,
+//       lng: 77.01502627,
+//     },
+//     zoom: 11,
+//   };
+
+//   return (
+//     // Important! Always set the container height explicitly
+//     <div className={style.MapGoogle}>
+//       <GoogleMapReact
+//         bootstrapURLKeys={{ key: "" }}
+//         defaultCenter={defaultProps.center}
+//         yesIWantToUseGoogleMapApiInternals
+//         defaultZoom={defaultProps.zoom}
+//       >
+//         <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+//       </GoogleMapReact>
+//     </div>
+//   );
+// };
